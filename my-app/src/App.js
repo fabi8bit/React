83f8117c9eb7c79d723b1f400196f23c40842b85
fabi8bit <fabi8bit@gmail.com>
2023-08-02 06:18:34 +0000

    Add handle event in functional and class components

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,15 +5,19 @@ import FunctionalGreetingWithProps from './components/FunctionalGreetingWithProp
 import StatefulGreeting from './components/StatefulGreeting';
 import StatefulGreetingWithCallback from './components/StatefulGreetingWithCallback';
 import StatefulGreetingWithPrevState from './components/StatefulGreetingWithPrevState';
+import EventsFunctional from './components/EventsFunctional';
+import EventsClass from './components/EventsClass';
 
 function App() {
   return (
     <div className="App">
-      <FunctionalGreeting />
+      {/* <FunctionalGreeting />
       <FunctionalGreetingWithProps greeting="nice to meet you" name="Mikele" age="48" />
       <StatefulGreeting greeting="I'm a stateful class component" name="Mike" />
       <StatefulGreetingWithCallback greeting="I'm a stateful class component with callback" name="Mike" />
-      <StatefulGreetingWithPrevState greeting="I'm a stateful class component with PrevState" name="Mike" />
+      <StatefulGreetingWithPrevState greeting="I'm a stateful class component with PrevState" name="Mike" /> */}
+      <EventsFunctional />
+      <EventsClass />
     </div>
   );
 }
