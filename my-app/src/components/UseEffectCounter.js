2aa5ef564d1584de5cc3a8a767978458b0ed4e87
fabi8bit <fabi8bit@gmail.com>
2023-08-09 13:16:40 +0000

    Running Effects only once

--- my-app/src/components/UseEffectCounter.js
+++ my-app/src/components/UseEffectCounter.js
@@ -2,6 +2,7 @@ import React, {useState, useEffect} from 'react'
 
 function UseEffectCounter() {
     const [count, setCount] = useState(0);
+    const [time, setTime] = useState(0);
     useEffect(() => {
         console.log('count 1 effect')
         document.title = count;
@@ -12,6 +13,13 @@ function UseEffectCounter() {
         console.log('count 2 effect')
         document.title = count2;
         }, [count2]);
+
+    useEffect(() => {
+        console.log('Creating Timer')
+        const interval = setInterval(() => {
+            setTime(time => time + 1)
+        }, 1000);
+    }, []) //passing an empty array makes the useEffect running only one time
     
   return (
     <div>
@@ -21,6 +29,7 @@ function UseEffectCounter() {
         <button onClick={
             () => setCount2(count2 => count2 + 1)}>Increment count 2 ({count2})
         </button>
+        <h2>Time is {time}</h2>
     </div>
   )
 }
